1e221791d252006258648bc3d51d5dd1bece5f60
ssalgaga <ssalgaga@naver.com>
2024-03-11 10:10:52 +0900

    subnet

--- main.tf
+++ main.tf
@@ -22,38 +22,25 @@ resource "aws_vpc" "default" {
 
 # Create a Public Subnet
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id     = aws_vpc.default.id
-  cidr_block = "10.0.100.0/24"
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.100.0/24"
+  availability_zone = "us-east-2a"
 
   tags = {
     Name = "hangaramit_public_subnet_1"
   }
 }
 
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id     = aws_vpc.default.id
-  cidr_block = "10.0.101.0/24"
-
-  tags = {
-    Name = "hangaramit_public_subnet_2"
-  }
-}
-
 # Create a Private Subnet
 resource "aws_subnet" "private_subnet_1" {
-  vpc_id     = aws_vpc.default.id
-  cidr_block = "10.0.110.0/24"
-
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = "10.0.101.0/24"
+  availability_zone = "us-east-2b"
   tags = {
     Name = "hangaramit_private_subnet_1"
   }
 }
 
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id     = aws_vpc.default.id
-  cidr_block = "10.0.111.0/24"
-
-  tags = {
-    Name = "hangaramit_private_subnet_2"
-  }
+data "aws_subnet" "private_subnet_1" {
+  id = "subnet-0ade8f7ec79c514f5"
 }
