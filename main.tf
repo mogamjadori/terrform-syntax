936b9b8b5e7c7931a6f13bfe138485aa057b33f2
ssalgaga <ssalgaga@naver.com>
2024-03-11 10:29:55 +0900

    작업완료

--- main.tf
+++ main.tf
@@ -12,35 +12,13 @@ provider "aws" {
   region = "us-east-2"
 }
 
-# Create a VPC
-resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "main"
-  }
+# 개발환경
+module "default_custom_vpc" {
+  source = "./custom_vpc"
 }
 
-# Create a Public Subnet
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = aws_vpc.default.id
-  cidr_block        = "10.0.100.0/24"
-  availability_zone = "us-east-2a"
-
-  tags = {
-    Name = "hangaramit_public_subnet_1"
-  }
-}
-
-# Create a Private Subnet
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = aws_vpc.default.id
-  cidr_block        = "10.0.101.0/24"
-  availability_zone = "us-east-2b"
-  tags = {
-    Name = "hangaramit_private_subnet_1"
-  }
-}
-
-data "aws_subnet" "private_subnet_1" {
-  id = "subnet-0ade8f7ec79c514f5"
+# 운영환경
+# 개발환경
+module "prd_custom_vpc" {
+  source = "./custom_vpc"
 }
